3a06628c5fb3e8fcc18fad880a00588c77919540
Stephan Schaefer <stephan@cloudtales.de>
2019-11-13 14:46:55 +0100

    Inital commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Amplify & CDN Test"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { components } from 'aws-amplify-vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    ...components
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Amplify, * as AmplifyModules from 'aws-amplify'
+import { AmplifyPlugin } from 'aws-amplify-vue'
+import awsconfig from './aws-exports'
 
 Vue.config.productionTip = false
 
+Amplify.configure(awsconfig)
+
+Vue.use(AmplifyPlugin, AmplifyModules)
+
+// It's important that you instantiate the Vue instance after calling Vue.use!
+
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
